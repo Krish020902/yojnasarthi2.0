503c289833b451ffc448b251cb13c801b6ad2639
krish <krish.mehta.3822@gmail.com>
2022-10-08 10:44:28 +0530

    new web

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
-import { useSchemeContext } from './context/context';
-import Navbar from './components/Navbar';
-import Slider from './components/Slider';
-import Section from './components/Section';
+import logo from "./logo.svg";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Slider from "./components/Slider";
+import Section from "./components/Section";
+import SingleScheme from "./components/SingleScheme";
+import LandingScheme from "./components/LandingScheme";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+
+import Bot from "./components/Bot";
 
 function App() {
   // const {temp} = useSchemeContext();
   return (
-    <div className="App">
-      {/* <> */}
-      <Navbar />
-      <Slider />
-      {/* </> */}
-      <div className="section">
-        <Section />
-      </div>
+    //Main Page and Chatbot
+    // <div className="App">
+    //   {/* <> */}
+    //   <Navbar />
+    //   <Slider />
+    //   {/* </> */}
+    //   <div className="section">
+    //     <Section />
+    //   </div>
+    //   <div>
+    //     <Bot />
+    //   </div>
+    // </div>
 
-    </div>
+    //Landing Page
+    // <div className="App">
+    //   <Navbar />
+    //   {/* <SingleScheme /> */}
+    //   <Bot />
+    //   <LandingScheme />
+    // </div>
+
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/oneScheme" element={<LandingScheme />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,68 +1,75 @@
 import React from "react";
 import "../index.css";
+import "../App.css";
 const Section = () => {
   return (
     <>
-      <div className="flex flex-col pb-20 main">
-        <div className="items-start flex mx-12">
-          <h1 className="font-sans text-4xl font-bold mb-10 mt-5">
-            {" "}
-            How to Search ?{" "}
-          </h1>
+      <div className="section">
+        <div className="flex flex-col pb-20 main">
+          <div className="items-start flex mx-12">
+            <h1 className="font-sans text-4xl font-bold mb-10 mt-5">
+              {" "}
+              {/* How to Search ?{" "} */}
+              कैसे खोजें?{" "}
+            </h1>
+          </div>
+
+          <div className="flex flex-row justify-around section">
+            <div className="flex justify-between py-6 p-4 w-[30%] container bg-white rounded-md  shadow-2xl ...">
+              <div className="w-[40%]">
+                <p className="text-xl font-bold text-start">
+                  {" "}
+                  {/* Enter your details{" "} */}
+                  अपना विवरण दर्ज करें{" "}
+                </p>
+              </div>
+              <div className="w-[60%]">
+                <img
+                  src="./img3.jpg"
+                  alt=""
+                  className="ml-8 w-[80%] h-[100%] rounded-xl"
+                />
+              </div>
+            </div>
+
+            <div className="flex justify-between py-6 p-4 w-[30%] container bg-white rounded-md shadow-2xl ...">
+              <div className="w-[40%]">
+                <p className="text-xl font-bold text-start">
+                  {/* Filter out your schemes */}
+                  अपनी योजनाओं को फ़िल्टर करें
+                </p>
+              </div>
+              <div className="w-[60%]">
+                <img
+                  src="./img2.jpg"
+                  alt=""
+                  className=" ml-8 w-[80%] h-[100%] rounded-xl"
+                />
+              </div>
+            </div>
+
+            <div className="flex justify-between py-6 p-4 w-[30%] container bg-white rounded-md shadow-2xl ...">
+              <div className="w-[40%]">
+                <p className="text-xl font-bold text-start">
+                  {/* Apply for your schemes */}
+                  अपनी योजनाओं के लिए आवेदन करें
+                </p>
+              </div>
+              <div className="w-[60%]">
+                <img
+                  src="./img1.jpeg"
+                  alt=""
+                  className="w-[80%] h-[100%] ml-8 rounded-xl"
+                />
+              </div>
+            </div>
+          </div>
         </div>
 
-        <div className="flex flex-row justify-around section">
-          <div className="flex justify-between py-6 p-4 w-[30%] container bg-white rounded-md  shadow-2xl ...">
-            <div className="w-[40%]">
-              <p className="text-xl font-bold text-start">
-                {" "}
-                Enter your details{" "}
-              </p>
-            </div>
-            <div className="w-[60%]">
-              <img
-                src="./img3.jpg"
-                alt=""
-                className="ml-8 w-[80%] h-[100%] rounded-xl"
-              />
-            </div>
-          </div>
-
-          <div className="flex justify-between py-6 p-4 w-[30%] container bg-white rounded-md shadow-2xl ...">
-            <div className="w-[40%]">
-              <p className="text-xl font-bold text-start">
-                Filter out your schemes
-              </p>
-            </div>
-            <div className="w-[60%]">
-              <img
-                src="./img2.jpg"
-                alt=""
-                className=" ml-8 w-[80%] h-[100%] rounded-xl"
-              />
-            </div>
-          </div>
-
-          <div className="flex justify-between py-6 p-4 w-[30%] container bg-white rounded-md shadow-2xl ...">
-            <div className="w-[40%]">
-              <p className="text-xl font-bold text-start">
-                Apply for your schemes
-              </p>
-            </div>
-            <div className="w-[60%]">
-              <img
-                src="./img1.jpeg"
-                alt=""
-                className="w-[80%] h-[100%] ml-8 rounded-xl"
-              />
-            </div>
-          </div>
-        </div>
+        <button className="bg-[#FEB347] hover:bg-orange-500 text-white font-bold px-16 py-4 rounded-full mb-10 ">
+          Enter Your Details
+        </button>
       </div>
-
-      <button className="bg-[#FEB347] hover:bg-blue-700 text-white font-bold px-16 py-4 rounded-full mb-10 ">
-        Enter Your Details
-      </button>
     </>
   );
 };
